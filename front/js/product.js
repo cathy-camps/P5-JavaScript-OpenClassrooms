4538f6330fcf459920396ac150912dc715f37fd9
Cathy Camps <cathycamps@live.fr>
2022-10-22 15:07:20 +0200

    link product page to homepage

--- front/js/product.js
+++ front/js/product.js
@@ -1,14 +1,24 @@
 //récupérer l'URL de la page produit
-const productUrl = window.location.href;
-const urlParams = new URL(productUrl);
-const idParams = urlParams.searchParams.get("id");
+let productURL = window.location.href;
+let urlParams = new URL (productURL);
+let idParams = urlParams.searchParams.get("id");
 console.log(idParams)
 
-//récupérer l'id du produit à afficher
-  fetch("http://localhost:3000/api/products/" + idParams)
-  .then((res) => res.json())
-  console.log("success")
-  .then((product) => { //insertion des éléments selon leur id
-    document.querySelector(".item__img").innerHTML = 'img src="${product.imageUrl}", alt="${product.altTxt}"`;
-)
-}
+//récupérer l'id du produit cliqué dans l'API
+async function fetchProduct(_id) {
+  try {
+  const res = await
+  fetch("http://localhost:3000/api/products/")
+  if (!res.ok) {
+    throw new Error (`HTTP error : ${res.status}`);
+  }
+  const product = await res.json();
+  return product;
+  }
+  catch (error) {
+    console.error(`Une erreur est survenue !: ${error}`)
+  }
+  }
+const promise = fetchProduct();
+promise.then((product) => console.log(product[0]._id))
+
