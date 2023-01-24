06145722e36a58f786a9a1abc934ec880d2365fb
Cathy Camps <cathycamps@live.fr>
2023-01-24 13:56:29 +0100

    add comments code

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,4 +1,6 @@
+//Récupérer l'id de commande d'un client 
 const urlValue = window.location.search;
+//Extraire l'id de commande de l'URL actuelle
 const urlParams = new URLSearchParams(urlValue);
 const numberOfCommand = urlParams.get("orderId");
 document.getElementById("orderId").textContent = numberOfCommand + 'Merci pour votre commande';
